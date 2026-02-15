687110658e2d6ef378458e2672bbbd710a28ab1d
Sumit Garg <sg434573@gmail.com>
2026-02-15 13:55:51 +0530

    Add server listening on specified PORT  6-9

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log("Server running");
+});
+
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
